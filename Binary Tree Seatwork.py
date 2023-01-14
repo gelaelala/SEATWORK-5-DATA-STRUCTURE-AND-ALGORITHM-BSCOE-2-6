777a05616c86199bda7417c28c2b3e984063bd4a
unknown <angelaescariocorpuz@gmail.com>
2023-01-14 12:46:04 +0800

    added add child method inside the class and some if statements

--- Binary Tree Seatwork.py
+++ Binary Tree Seatwork.py
@@ -5,3 +5,13 @@ class BinarySearchTreeNode:
         self.data = data
         self.left = None
         self.right = None
+
+    def add_child (self, data):
+        if data == self.data:
+            return
+        
+        if data < self.data:
+            if self.left:
+                self.left.add_child (data)
+            else:
+                self.left = BinarySearchTreeNode (data)
