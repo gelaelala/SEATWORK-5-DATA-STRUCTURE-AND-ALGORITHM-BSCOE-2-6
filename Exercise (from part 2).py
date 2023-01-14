8d8d3206b6f765cae2011b9b3c00a45059c422be
unknown <angelaescariocorpuz@gmail.com>
2023-01-14 12:13:35 +0800

    added find min method inside the class in order to find the minimum element from the tree

--- Exercise (from part 2).py
+++ Exercise (from part 2).py
@@ -53,4 +53,7 @@ class BinarySearchTreeNode:
             return self.data
         return self.right.find_max()
 
-        
+    def find_min (self):
+        if self.left is None:
+            return self.data
+        return self.left.find_min()
