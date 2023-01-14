b38e3e69291499b30d34b8af4a0d81d2c0a16069
unknown <angelaescariocorpuz@gmail.com>
2023-01-14 10:55:47 +0800

    added find_min method in order to find the minimum element within the tree

--- Binary Tree (part 2).py
+++ Binary Tree (part 2).py
@@ -53,6 +53,11 @@ class BinarySearchTreeNode:
             return self.data
         return self.right.find_max()
 
+    def find_min (self):
+        if self.left is None:
+            return self.data
+        return self.left.find_min()
+
 def build_tree(elements):
     root = BinarySearchTreeNode(elements[0])
 
