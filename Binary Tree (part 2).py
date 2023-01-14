7fef94f46783b34a76622e10a45cec946b64e1f3
unknown <angelaescariocorpuz@gmail.com>
2023-01-14 11:00:56 +0800

    added if statements as conditions under delete method

--- Binary Tree (part 2).py
+++ Binary Tree (part 2).py
@@ -59,6 +59,22 @@ class BinarySearchTreeNode:
         return self.left.find_min()
 
     def delete (self, val):
+        if val < self.data:
+            if self.left:
+                self.left.delete(val)
+        elif val > self.data:
+            if self.right:
+                self.right.delete(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            if self.left is None:
+                return self.right
+            if self.right is None:
+                return self.right
+            
+            
+            
 
 def build_tree(elements):
     root = BinarySearchTreeNode(elements[0])
