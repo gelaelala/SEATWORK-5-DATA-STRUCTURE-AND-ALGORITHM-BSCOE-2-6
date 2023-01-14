003587c6b1dade3e8958cb91d56af3e668ca5848
unknown <angelaescariocorpuz@gmail.com>
2023-01-14 13:03:59 +0800

    added search method inside the class

--- Binary Tree Seatwork.py
+++ Binary Tree Seatwork.py
@@ -92,4 +92,20 @@ class BinarySearchTreeNode:
             self.right = self.right.delete(min_val)
 
         return self
+
+    def search (self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+            
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
             
